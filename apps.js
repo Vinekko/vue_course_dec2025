b3ecb0017e871ecff2ff64d22a74ff858f094ef7
Vincenzo Trigila <nitomaco@gmail.com>
2025-12-06 17:03:25 -0400

    methods added

--- apps.js
+++ apps.js
@@ -3,6 +3,17 @@ data() {
     return {
     tareasA: 'Lista de tareas',
     tareasB: 'Lista de compras',
+    vueLink: 'https://vuejs.org',
+    }
+},
+methods: {
+    changeTareas() {    
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+            return this.tareasA;
+        } else {
+            return this.tareasB;
+        }
     }
 }
 })
